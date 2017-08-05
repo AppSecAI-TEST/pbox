edd12cd03e6412050ea71c6892cba852d4515b68
ngLiaXL <344212231@qq.com>
2017-08-05 20:48:14 +0800

    icon commit

--- app/src/main/java/com/ldroid/pbox/entities/out/ChartOutEntity.java
+++ app/src/main/java/com/ldroid/pbox/entities/out/ChartOutEntity.java
@@ -4,4 +4,6 @@ package com.ldroid.pbox.entities.out;
  * Created by xianglong.liang on 2017/7/31.
  */
 public class ChartOutEntity {
+
+    public boolean header ;
 }
--- app/src/main/java/com/ldroid/pbox/module/main/fragment/ChartFragment.java
+++ app/src/main/java/com/ldroid/pbox/module/main/fragment/ChartFragment.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.ldroid.pbox.R;
 import com.ldroid.pbox.common.ui.BaseFragment;
 import com.ldroid.pbox.common.ui.adapter.CommonAdapter;
+import com.ldroid.pbox.common.ui.adapter.MultiItemCommonAdapter;
+import com.ldroid.pbox.common.ui.adapter.MultiItemTypeSupport;
 import com.ldroid.pbox.common.ui.adapter.ViewHolder;
 import com.ldroid.pbox.common.ui.lib.swipemenulistview.SwipeMenu;
 import com.ldroid.pbox.common.ui.lib.swipemenulistview.SwipeMenuCreator;
@@ -34,7 +37,7 @@ public class ChartFragment extends BaseFragment {
 
 
     @BindView(R.id.list_view)
-    StickyListHeadersListView mListView;
+    SwipeMenuListView mListView;
     private Adapter mAdapter;
 
     @Override
@@ -53,10 +56,39 @@ public class ChartFragment extends BaseFragment {
         mAdapter = new Adapter();
         mListView.setAdapter(mAdapter);
 
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                createMenu2(menu);
+            }
+
+            private void createMenu2(SwipeMenu menu) {
+
+                SwipeMenuItem item = new SwipeMenuItem(getActivity());
+                item.setBackground(new ColorDrawable(Color.rgb(0xF9, 0x3F, 0x25)));
+                item.setWidth(DensityUtils.dp2px(getActivity(),90));
+                item.setTitle("删除");
+                item.setTitleSize(18);
+                item.setTitleColor(Color.WHITE);
+                menu.addMenuItem(item);
+
+            }
+        };
+        mListView.setMenuCreator(creator);
+
 
         // test
         ArrayList list = new ArrayList();
+        ChartOutEntity c  = new ChartOutEntity() ;
+        c.header = true ;
+        list.add(c);
         for (int i = 0; i < 20; i++) {
+            if(i == 5){
+                ChartOutEntity c1  = new ChartOutEntity() ;
+                c1.header = true ;
+                list.add(c1);
+
+            }
             list.add(new ChartOutEntity());
         }
         mAdapter.setListData(list);
@@ -81,43 +113,51 @@ public class ChartFragment extends BaseFragment {
         }
     }
 
-    class Adapter extends CommonAdapter<ChartOutEntity> implements StickyListHeadersAdapter {
+    class Adapter extends MultiItemCommonAdapter<ChartOutEntity> {
 
         public Adapter() {
-            super(getActivity(), R.layout.layout_chart_item);
+            super(getActivity(), new MultiItemTypeSupport<ChartOutEntity>() {
+                @Override
+                public int getLayoutId(int position, ChartOutEntity item) {
+                    if(item.header){
+                        return R.layout.layout_sticky_header_chart;
+                    }
+                    return R.layout.layout_chart_item;
+                }
+
+                @Override
+                public int getViewTypeCount() {
+                    return 2;
+                }
+
+                @Override
+                public int getItemViewType(int postion, ChartOutEntity item) {
+                    if(item.header){
+                        return 0 ;
+                    }
+                    return 1;
+                }
+            });
         }
 
         @Override
         public void convert(ViewHolder holder, ChartOutEntity chartOutEntity) {
-
-
-        }
-
-        @Override
-        public View getHeaderView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.layout_sticky_header_chart, parent, false);
+            switch (holder.getLayoutId()) {
+                case R.layout.layout_sticky_header_chart:
+                    holder.setOnClickListener(R.id.tv_export,new View
+                            .OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startAnimActivity(new Intent(getActivity(), ChartExoprtActivity.class));
+                        }
+                    });
+                    break ;
+                case R.layout.layout_chart_item:
+                    break ;
             }
 
-            long section = getHeaderId(position);
-            TextView tv = (TextView) convertView.findViewById(R.id.title);
-            tv.setText("本月" + section);
-
-            convertView.findViewById(R.id.tv_export).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startAnimActivity(new Intent(getActivity(), ChartExoprtActivity.class));
-                }
-            });
-
-
-            return convertView;
         }
 
-        @Override
-        public long getHeaderId(int position) {
-            return position % 5;
-        }
     }
 
 

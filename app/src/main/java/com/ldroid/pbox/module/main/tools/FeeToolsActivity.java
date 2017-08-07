b8ddcafbbebc9402d4eaba335cf496e6e6d1884e
ngLiaXL <344212231@qq.com>
2017-08-07 16:34:04 +0800

    logistics tools commit

--- app/src/main/java/com/ldroid/pbox/module/main/tools/FeeToolsActivity.java
+++ app/src/main/java/com/ldroid/pbox/module/main/tools/FeeToolsActivity.java
@@ -1,12 +1,25 @@
 package com.ldroid.pbox.module.main.tools;
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.ldroid.pbox.R;
 import com.ldroid.pbox.common.ui.BaseActivity;
+import com.ldroid.pbox.widget.BasePopupWindow;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 
 public class FeeToolsActivity extends BaseActivity {
 
 
+    @BindView(R.id.tv_edit_channel)
+    TextView mTvChannel;
+    @BindView(R.id.root)
+    View mRoot;
+
+    private FeeToolsPopupWindow mPopWindow;
 
 
     @Override
@@ -25,7 +38,20 @@ public class FeeToolsActivity extends BaseActivity {
 
         initTopBarForLeft("运费估算", null, getResources().getDrawable(R.drawable.icon_common_back));
 
+        mPopWindow = new FeeToolsPopupWindow(this);
+        mPopWindow.setListener(new BasePopupWindow.OnPopupWindowItemSelectedListener() {
+            @Override
+            public void onItemSelected(Object... obj) {
+                mTvChannel.setText(String.valueOf(obj[0]));
+                mTvChannel.setTag(obj[1]);
+            }
+        });
+    }
 
+
+    @OnClick(R.id.tv_edit_channel)
+    public void onClickEdit() {
+        mPopWindow.showFromBottom(mRoot);
     }
 
     @Override
@@ -37,7 +63,6 @@ public class FeeToolsActivity extends BaseActivity {
     protected void initData() {
 
     }
-
 
 
 }

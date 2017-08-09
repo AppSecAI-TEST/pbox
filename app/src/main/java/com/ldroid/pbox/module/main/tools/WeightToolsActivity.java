fb284c9dbfc5f2a4fd6cfb7f2977f161b934fcb6
ngLiaXL <344212231@qq.com>
2017-08-09 17:10:32 +0800

    API tools commit

--- app/src/main/java/com/ldroid/pbox/module/main/tools/WeightToolsActivity.java
+++ app/src/main/java/com/ldroid/pbox/module/main/tools/WeightToolsActivity.java
@@ -1,17 +1,46 @@
 package com.ldroid.pbox.module.main.tools;
 
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.ldroid.pbox.R;
 import com.ldroid.pbox.common.ui.BaseActivity;
+import com.ldroid.pbox.common.util.ToastUtils;
+import com.ldroid.pbox.dao.ConfigDao;
+import com.ldroid.pbox.entities.out.ToolsCBLROutEntity;
+import com.ldroid.pbox.entities.out.ToolsCountrysOutEntity;
+import com.ldroid.pbox.entities.out.ToolsEMSOutEntity;
+import com.ldroid.pbox.entities.out.ToolsEYBOutEntity;
+import com.ldroid.pbox.entities.out.ToolsGHDBOutEntity;
+import com.ldroid.pbox.entities.out.ToolsGHXBOutEntity;
+import com.ldroid.pbox.entities.out.ToolsHGZXOutEntity;
+import com.ldroid.pbox.entities.out.ToolsHWCOutEntity;
+import com.ldroid.pbox.entities.out.ToolsPZHSOutEntity;
+import com.ldroid.pbox.entities.out.ToolsTYOutEntity;
+import com.ldroid.pbox.entities.out.ToolsWLZZOutEntity;
+import com.ldroid.pbox.entities.out.ToolsYFGSOutEntity;
+import com.ldroid.pbox.entities.out.UserOutEntity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 
-public class WeightToolsActivity extends BaseActivity {
+public class WeightToolsActivity extends BaseActivity implements  ToolsContract.View{
+
+    private ToosPresenter mPresenter;
 
 
+    @BindView(R.id.length) EditText length ;
+    @BindView(R.id.width) EditText width ;
+    @BindView(R.id.height) EditText height ;
+    @BindView(R.id.countingbase) EditText countingbase ;
+    @BindView(R.id.result) TextView result ;
 
 
     @Override
     protected void initPreparation() {
-
+        mPresenter = new ToosPresenter(this) ;
     }
 
     @Override
@@ -28,6 +57,21 @@ public class WeightToolsActivity extends BaseActivity {
 
     }
 
+
+    @OnClick(R.id.btn_search)
+    public void onClickSearch(){
+        UserOutEntity user = ConfigDao.getInstance().getUser();
+        String userid = user != null ? user.UserID : null;
+        String length = this.length.getText().toString();
+        String width = this.width.getText().toString();
+        String height = this.height.getText().toString();
+        String countingbase = this.countingbase.getText().toString();
+        mPresenter.reqToolsPZHS(userid, length, width, height, countingbase);
+    }
+
+
+
+
     @Override
     protected void initListener() {
 
@@ -39,5 +83,83 @@ public class WeightToolsActivity extends BaseActivity {
     }
 
 
+    @Override
+    public Context getContext() {
+        return this;
+    }
 
+    @Override
+    public void showLoading(String msg) {
+        showProgressDialog(msg);
+    }
+
+    @Override
+    public void dismissLoading() {
+        dismissPorgressDialog();
+    }
+
+    @Override
+    public void onError(String msg) {
+        ToastUtils.showLongToast(mContext,msg);
+    }
+
+    @Override
+    public void onRespToolsTY(ToolsTYOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsEYB(ToolsEYBOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsGHXB(ToolsGHXBOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsGHDB(ToolsGHDBOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsEMS(ToolsEMSOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsHWC(ToolsHWCOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsCountry(ToolsCountrysOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsCBLR(ToolsCBLROutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsPZHS(ToolsPZHSOutEntity data) {
+        result.setText("抛重：" + data.Calculation + "KG");
+    }
+
+    @Override
+    public void onRespToolsHGZX(ToolsHGZXOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsYFGS(ToolsYFGSOutEntity data) {
+
+    }
+
+    @Override
+    public void onRespToolsWLZZ(ToolsWLZZOutEntity data) {
+
+    }
 }
